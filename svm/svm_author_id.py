8d469e12b8df85244423d6a1da5e2de5d58484e2
Nguyen Gia Luan <luanng@LAP10817.(none)>
2016-04-14 14:02:39 +0700

    add visual into decision tree

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,29 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+
+clf = SVC(C=10000,kernel='rbf')
+
+t0 = time()
+
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+
+print "training time:",time()-t0, "s"
+
+t1=time()
+pred = clf.predict(features_test)
+print "predict time:",time()-t1, "s"
+print pred
+
+from sklearn.metrics import accuracy_score
+
+acc = accuracy_score(pred, labels_test)
+print acc
+
+#0.89
 #########################################################
 
 
